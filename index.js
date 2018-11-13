01effaa6e05fe83ba012f521e0675c5f58f80bd0
ArtemAntonenko <artem.antonenko.kpi1@gmail.com>
2018-11-13 23:46:46 +0200

    test simple bot

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 process.env.NTBA_FIX_319 = 1
+const TOKEN = process.env.TOKEN;
 const TelegramBot = require('node-telegram-bot-api');
-
-const TOKEN = '623206542:AAG-Us9W2uJkNpvtIbzfqisJDBIdIDQhsow';
 const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 
-const setListenersToBot = require('./controllers')
+const setListeners = require('./controllers')
 
-setListenersToBot(bot)
+setListeners(bot)
