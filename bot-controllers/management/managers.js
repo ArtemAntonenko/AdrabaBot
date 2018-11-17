1e75cc6a0acab36deb6f83e207868fcfb4da79c3
ArtemAntonenko <artem.antonenko.kpi1@gmail.com>
2018-11-17 15:01:03 +0200

    management controller - add ceo

--- bot-controllers/management/managers.js
+++ bot-controllers/management/managers.js
@@ -9,13 +9,8 @@ const AriPhoto = fs.readFileSync('./assets/management/Ari.png');
 module.exports = {
     CEO: {
         photo: JosephPhoto,
-        about: `<b>Joseph Roash</b> \n<b>CEO & Founder</b> \nJoseph brings more than 18 years
-            of software development and project management experience to his leadership role at Adraba. 
-            From being a co-founder of Leverate, a leading provider of software and financial solutions
-            to the online forex industry, to becoming the R&D Site Manager and taking projects from
-            inception to deployment, to being an Honor alumni of the IDF Mamram Programming Course
-            and having served in an elite unit of the Israeli Intelligence Services, Joseph brings
-            to Adraba more than 18 years of software development and project management experience.`
+        about: `<b>Joseph Roash</b> \n<b>CEO & Founder</b> \nJoseph brings more than 18 years of software development and project management experience to his leadership role at Adraba. 
+From being a co-founder of Leverate, a leading provider of software and financial solutions to the online forex industry, to becoming the R&D Site Manager and taking projects from inception to deployment, to being an Honor alumni of the IDF Mamram Programming Course and having served in an elite unit of the Israeli Intelligence Services, Joseph brings to Adraba more than 18 years of software development and project management experience.`
     },
     COO: {
         photo: YuliaPhoto
