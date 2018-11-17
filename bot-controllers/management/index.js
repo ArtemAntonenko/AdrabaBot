c828a2f3c3fc536b37176a08b1c734e664262e13
ArtemAntonenko <artem.antonenko.kpi1@gmail.com>
2018-11-17 15:19:36 +0200

    management controller - add coo

--- bot-controllers/management/index.js
+++ bot-controllers/management/index.js
@@ -5,7 +5,9 @@ module.exports = (bot) => {
         await bot.sendPhoto(msg.chat.id, managers.CEO.photo)
         await bot.sendMessage(msg.chat.id, managers.CEO.about, { parse_mode: "HTML" })
         
-        // await bot.sendPhoto(msg.chat.id, managers.COO.photo)
+        await bot.sendPhoto(msg.chat.id, managers.COO.photo)
+        await bot.sendMessage(msg.chat.id, managers.COO.about, { parse_mode: "HTML" })
+
         // await bot.sendPhoto(msg.chat.id, managers.HRDirector.photo)
         // await bot.sendPhoto(msg.chat.id, managers.HeadOfProjects.photo)
         // await bot.sendPhoto(msg.chat.id, managers.Advisor.photo)
