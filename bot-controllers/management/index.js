5a1a14718568b36c5e7f8dab5624da2c87b88145
ArtemAntonenko <artem.antonenko.kpi1@gmail.com>
2018-11-19 23:38:07 +0200

    mangement - refactoring

--- bot-controllers/management/index.js
+++ bot-controllers/management/index.js
@@ -1,20 +1,32 @@
 const managers = require('./managers')
 
+const msgOptions = { parse_mode: "HTML", disable_notification: true }
+
 module.exports = (bot) => {
     bot.onText(/\/management/, async (msg, match) => {
-        await bot.sendPhoto(msg.chat.id, managers.CEO.photo)
-        await bot.sendMessage(msg.chat.id, managers.CEO.about, { parse_mode: "HTML" })
-        
-        await bot.sendPhoto(msg.chat.id, managers.COO.photo)
-        await bot.sendMessage(msg.chat.id, managers.COO.about, { parse_mode: "HTML" })
-
-        await bot.sendPhoto(msg.chat.id, managers.HRDirector.photo)
-        await bot.sendMessage(msg.chat.id, managers.HRDirector.about, { parse_mode: "HTML" })
-
-        await bot.sendPhoto(msg.chat.id, managers.HeadOfProjects.photo)
-        await bot.sendMessage(msg.chat.id, managers.HeadOfProjects.about, { parse_mode: "HTML" })
-        
-        await bot.sendPhoto(msg.chat.id, managers.Advisor.photo)
-        await bot.sendMessage(msg.chat.id, managers.Advisor.about, { parse_mode: "HTML" })
+        await bot.sendMessage(msg.chat.id, getManagersList(), msgOptions)
     });
+
+    bot.on('message', async (msg, match) => {
+        const command = msg.text
+        const manager = managers.find(manager => manager.command === command)
+        if (manager) {
+            await bot.sendPhoto(msg.chat.id, manager.photo)
+            await bot.sendMessage(msg.chat.id, getManagerInfo(manager), msgOptions)
+        }
+    });
+}
+
+function getManagersList() {
+    return managers.reduce((acc, manager) => {
+        acc = `${acc}${manager.command} - ${manager.name} \n\n`
+        return acc
+    }, '<b>Adraba Managers:</b>\n\n')
+}
+
+function getManagerInfo(manager) {
+    return `<b>${manager.name}</b>
+     \n<b>${manager.position}</b>
+     \n\n${manager.about}
+     \n${getManagersList()}`
 }
--- bot-controllers/management/managers.js
+++ bot-controllers/management/managers.js
@@ -5,35 +5,40 @@ const TanyaPhoto = fs.readFileSync('./assets/management/Tanya.png');
 const AlbinaPhoto = fs.readFileSync('./assets/management/Albina.png');
 const AriPhoto = fs.readFileSync('./assets/management/Ari.png');
 
-module.exports = {
-    CEO: {
+module.exports = [
+    {
         photo: JosephPhoto,
         name: 'Joseph Roash',
         position: 'CEO & Founder',
+        command: '/CEO',
         about: 'Joseph brings more than 18 years of software development and project management experience to his leadership role at Adraba. From being a co-founder of Leverate, a leading provider of software and financial solutions to the online forex industry, to becoming the R&D Site Manager and taking projects from inception to deployment, to being an Honor alumni of the IDF Mamram Programming Course and having served in an elite unit of the Israeli Intelligence Services, Joseph brings to Adraba more than 18 years of software development and project management experience.'
     },
-    COO: {
+    {
         photo: YuliaPhoto,
         name: 'Yulia Martynova',
         position: 'COO',
+        command: '/COO',
         about: `As COO of Adraba, Yulia is responsible of overseeing various departments within the company, from marketing and customer support to administration. Throughout her career, Yulia has helped some of the world’s most prominent businesses find the best resources available in Ukraine, in order for them to establish and optimize their businesses. In her spare time, Yulia puts her London College of Fashion degree to good use by giving lectures to some of the top retailers in Ukraine.`
     },
-    HRDirector: {
+    {
         photo: TanyaPhoto,
         name: 'Tanya Shevchuk',
         position: 'HR Director',
+        command: '/HR_Director',
         about: `Tanya offers 7 years of experience in the direction of HR and business development. The main areas of activity are analysis, construction and implementation of the personnel management system; analysis and implementation of business processes; development and implementation of corporate culture. Also has a successful experience as HR and business development Advisor in leading companies in such areas as: manufacturing, retail, agro-trading, fuel industry and others.`
     },
-    HeadOfProjects: {
+    {
         photo: AlbinaPhoto,
         name: 'Albina Kravchenko',
         position: 'Head of Projects',
+        command: '/Head_of_Projects',
         about: `Responsible for Delivery. About 10 years in software development in leading companies. The experience in Quality, Development and Project Management areas allows to lead continuous improvements of projects production. Certified as Project Management Professional by Project Management Institute.`
     },
-    Advisor: {
+    {
         photo: AriPhoto,
         name: 'Ari Shpanya',
-        position: 'Advisor', 
+        position: 'Advisor',
+        command: '/Advisor',
         about: `Co-founder at Zent.com and HomeShare.com, Previously founder and CEO at Wiser.com- an enterprise focused SaaS which grew from scratch to serve hundreds of customers. He is a guest blogger and been featured on Econsultancy, VentureBeat and Forbes. Ari holds a LL.B. (Magna Cum Laude) in Legal Studies from IDC Herzliya and is graduate of the Stanford Ignite program at the Stanford University Graduate School of Business.`
     }
-}
+]
