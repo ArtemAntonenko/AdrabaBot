671c50ead2e9b827e91a07733b2abfd44bcaf8d3
ArtemAntonenko <artem.antonenko.kpi1@gmail.com>
2018-11-20 00:41:13 +0200

    bot - refactoring

--- bot.js
+++ bot.js
@@ -3,18 +3,15 @@ process.env.NTBA_FIX_350 = 1
 
 const TelegramBot = require('node-telegram-bot-api');
 const setupBotControllers = require('./bot-controllers')
-// const TOKEN = process.env.TOKEN;
-const TOKEN = '623206542:AAG-Us9W2uJkNpvtIbzfqisJDBIdIDQhsow';
+const TOKEN = process.env.TOKEN;
 let bot
 
 if(process.env.NODE_ENV === 'production') {
-  console.log('Case 1')
   bot = new TelegramBot(TOKEN);
   const webhookUrl = process.env.HEROKU_URL + TOKEN
   bot.setWebHook(webhookUrl);
 }
 else {
-  console.log('Case 2')
   bot = new TelegramBot(TOKEN, { polling: true });
 }
 
