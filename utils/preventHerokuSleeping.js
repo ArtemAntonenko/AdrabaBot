8a01f5289d1f5e2edfb74056c4305dda2e751463
artem.antonenko <artem.antonenko@adraba.io>
2018-11-22 10:35:47 +0200

    prevent heroku sleeping - done

--- utils/preventHerokuSleeping.js
+++ utils/preventHerokuSleeping.js
@@ -1,9 +1,8 @@
 var https = require("https");
-const INTERVAL = 0.5 * 60 * 1000
+const INTERVAL = 25 * 60 * 1000
 
 module.exports = () => {
   setInterval(function() {
-    console.log('Prevent sleeping')
     https.get(process.env.HEROKU_URL);
   }, INTERVAL)
 };
